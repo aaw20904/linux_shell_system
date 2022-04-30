5101f7a383e3aaac9bc5c1f4ae4628d319a4cec2
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2022-04-30 09:54:13 +0300

    Update docker_mhddos.sh

--- docker_mhddos.sh
+++ docker_mhddos.sh
@@ -1,26 +1,26 @@
  #!/bin/bash
  sudo apt update
- SLEEP 2
+ SLEEP 1
 sudo apt install -y docker.io
-SLEEP 2
+SLEEP 1
 sudo systemctl enable docker --now
-SLEEP 2
+SLEEP 1
 docker
-SLEEP 2
+SLEEP 1
 sudo usermod -aG docker $USER
-SLEEP 2
+SLEEP 1
 printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list ; curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
- SLEEP 2
+ SLEEP 1
  sudo apt update
-SLEEP 2
+SLEEP 1
 sudo apt install -y docker-ce docker-ce-cli containerd.io
-SLEEP 2
+SLEEP 1
 sudo systemctl unmask docker.service
-SLEEP 2
+SLEEP 1
 sudo systemctl unmask docker.socket
-SLEEP 2
+SLEEP 1
 sudo systemctl start docker.service
-SLEEP 2
+SLEEP 1
 sudo chmod 666 /var/run/docker.sock
-SLEEP 2
+SLEEP 1
  sudo docker run hello-world
