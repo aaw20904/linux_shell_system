d23372193cdc61e75fb8a48830c0b1b1df4af817
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2022-04-30 09:49:23 +0300

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 free -h
+SLEEP 3
 df -h
+SLEEP 3
 sudo fallocate -l 1G /swapfile
+SLEEP 3
 ls -lh /swapfile
+SLEEP 3
 sudo chmod 600 /swapfile
+SLEEP 3
 ls -lh /swapfile
+SLEEP 3
 sudo mkswap /swapfile
+SLEEP 3
 sudo swapon /swapfile
+SLEEP 3
 free -h
+SLEEP 3
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
