79cc84cf6c75e0b72341de83b78a4e1cd5ad34cd
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2022-05-09 09:16:18 +0300

    Update python_mhddos.sh

--- python_mhddos.sh
+++ python_mhddos.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 sudo apt update -y
-SLEEP 1
+sleep 1
 sudo apt install --upgrade git -y
-SLEEP 1
+sleep 1
 sudo apt install --upgrade python3 -y
-SLEEP 1
+sleep 1
 sudo apt install --upgrade python3-pip -y
-SLEEP 1
+sleep 1
 sudo rm -r mhddos_proxy  //если было установлено ранее
-SLEEP 1
+sleep 1
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
-SLEEP 1
+sleep 1
 cd mhddos_proxy
-SLEEP 1
+sleep 1
 sudo python3 -m pip install -r requirements.txt
