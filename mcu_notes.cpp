03f214ee60c6c0c0717ea898f9776ea5a9c0a436
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2022-07-22 20:41:05 +0300

    Update mcu_notes.cpp

--- mcu_notes.cpp
+++ mcu_notes.cpp
@@ -53,3 +53,26 @@ struct interrupt_vector const _vectab[] = {
 		 GPIO_WriteReverse(GPIOB,GPIO_PIN_5 );
     TIM4_ClearITPendingBit(TIM4_IT_UPDATE);
 }
+/*
+6)At the end of a file 'main.c' add next function for assert:*/
+
+#ifdef USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param file: pointer to the source file name
+  * @param line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
