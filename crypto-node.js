dcf3c4e3ff6c2781e5e3341553bb52be7bae8503
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2022-08-09 09:42:31 +0300

    Update crypto-node.js

--- crypto-node.js
+++ crypto-node.js
@@ -1,1 +1,16 @@
 
+//------------------HMAC----------------------- 
+/******keyed-hash message authentication code****/
+//---an encrypted message building by a public key
+///---HMAC = 'my some message' + public key 
+  const crypto = require('crypto');
+  const pubKey = "password";
+  const message1 = 'helloWord';
+  const message2='helloWord';
+/***the first HMAC data**/
+
+const hmac1 = crypto.createHmac('sha256', pubKey).update(message1);
+const result1 = hmac1.digest('hex');
+/**the second HMAC data**/
+const hmac2 = crypto.createHmac('sha256', pubKey).update('message2);
+const result2 = hmac2.digest('hex');
