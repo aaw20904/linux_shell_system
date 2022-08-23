e1404e600fd4928767c87a432a0ac2f3b741b811
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2022-08-23 16:58:08 +0300

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -1,3 +1,48 @@
+/*******SCALLING IMAGE ***/
+
+////DOM elems
+ <label for="avatar" class="form-label maintextcolor">Avatar image:</label>
+ <input type="file" value="Choose JPG File" class="form-control maintextcolor" id="avatar"  placeholder="Select an image- your avatar" name="avatar"  accept="image/*"  required/>
+///function
+function convert (){
+return new Promise((resolve, reject) => {
+            // (A) GET SELECTED FILE
+            let imageFile = document.getElementById("avatar").files[0];
+                // (B) READ SELECTED FILE
+            let reader = new FileReader();
+            reader.addEventListener("load", (e) => {
+                let currentImageData = e.target.result;
+                //create an image
+                let img= document.createElement('img');
+                img.onload=(event)=>{
+                    // Dynamically create a canvas element
+                       var canvas = document.createElement("canvas");
+                       canvas.widtth=100;
+                        canvas.height=100;
+                    //actual resizing
+                        var ctx = canvas.getContext("2d");
+                         ctx.canvas.width=100;
+                         ctx.canvas.height=100;
+                    console.log(`Image wh: ${img.width},${img.height}`);
+                    // Actual resizing
+                         ctx.drawImage(img, 0, 0, 100, 100);
+                        
+                    //  convert to 'DataUrl' and assign it to a variable
+                        var dataurl = canvas.toDataURL(imageFile.type);
+                        document.getElementById("demoShow").src = dataurl;
+                    console.log(`Image wh: ${img.width},${img.height}`);
+                          resolve( dataurl);
+                }
+                img.src =  e.target.result;
+                reader.onerror=x=>reject(x);
+             // sendDataToServer(null,y);
+             //   document.getElementById("demoShow").src = reader.result;
+
+            });
+        reader.readAsDataURL(imageFile);
+    });
+    
+}
 /********SERVICE WORKER**********/
 //lifecycle - I)registration
 	if (! ('serviceWorker' in navigator)) {
