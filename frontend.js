38e097c2991cdad716a9e01f310fb558a2377d7d
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2022-08-27 14:35:11 +0300

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -1,3 +1,25 @@
+/******Buffer (Array) to Base64****************/
+function arrayBufferToBase64( buffer ) {
+	var binary = '';
+	var bytes = new Uint8Array( buffer );
+	var len = bytes.byteLength;
+	for (var i = 0; i < len; i++) {
+		binary += String.fromCharCode( bytes[ i ] );
+	}
+	return window.btoa( binary );
+}
+
+
+function base64ToArrayBuffer(base64) {
+    var binary_string =  window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array( len );
+    for (var i = 0; i < len; i++)        {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
 //***BOOTSTRAP validation template****/
 //all the validation runs inside bootstrap script
 <form action="/action_page.php" class="was-validated">
