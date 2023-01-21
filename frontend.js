52435a224955cf2b6639470fdcaab31078894ccd
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-01-21 16:04:10 +0200

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -136,5 +136,21 @@ self.addEventListener('push',
 	 event.waitUntil( self.registration.showNotification('Hello!',options));
   }
   )
-  
-  
+  //************************************************
+ /* bootstrap toast */
+<div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true" id="notificator">
+  <div class="d-flex">
+    <div class="toast-body">
+      Hello, world! This is a toast message.
+    </div>
+    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+  </div>
+</div>
+
+//--to show the toast - use JavaScript!
+//get  a toast
+var element = document.querySelector("#notificator");
+ // Create toast instance
+var myToast = new bootstrap.Toast(element);
+//show a toast
+ myToast.show();
