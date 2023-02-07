1c8c3257864aec494338c494fd81bfacbf9a19fe
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-02-07 12:51:31 +0200

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,1 +1,18 @@
 
+/*
+..add key-value pair:
+    set  status  1
+..get value by a key:
+    get status
+    1
+ ..remove pair:
+   del status
+ ..is the pair in system?
+   EXISTS my_key
+   1
+ ..
+ 
+ 
+  
+
+*/
