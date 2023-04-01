2b11ad3e70bee9ee00781470c5a57c11b589a067
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-04-01 14:36:50 +0300

    Update express_notes.js

--- express_notes.js
+++ express_notes.js
@@ -33,3 +33,34 @@ function myMiddleware1(req, resp, next){
 app.get('/', myMiddleware1, myMiddleware2, (req,res)=>{
   res.send("<h2>helloword</h2>");
 });
+
+/***when there was an exception during execution - it must be handled, because it
+leads to crash an express application. For example - it`s an error simulation:***/
+
+let app = express();
+app.use(myMiddleware1);
+app.use(myMiddleware2);
+
+function myMiddleware1(req, resp, next){
+  console.log(req.url);
+  next(new Error("It`s an error!"));
+}
+
+function myMiddleware2(req, resp, next){
+  console.log(req.url);
+  next();
+}
+
+app.get('/',(req,res)=>{
+  res.send("<h2>helloword</h2>");
+});
+
+/***to fix it there must be an error handler: ***/
+  //catching:
+app.use(errorHandler);
+  //processing:
+function errorHandler (err, req, res, next) {
+  if (err) {
+    res.send('<h1>there was an error!</h1>');
+  }
+}
