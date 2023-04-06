9b345ca4b2e337b37b71b8de794389829afc6e20
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-04-06 16:01:23 +0300

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,2 +1,8 @@
-/***to find all values of a collection**/
+/***show databases***/
+show dbs
+/**select the dbase*/
+use mybase
+/***show collections of key-value pair(s):**/
+show collections
+/***to find all values of the collection**/
 db.collection_name_here.find().pretty();
