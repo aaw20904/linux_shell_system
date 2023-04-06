5e9399e02337e1d1236dcc925ea8eb8c4a7afc68
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-04-06 20:37:29 +0300

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -6,3 +6,6 @@ use mybase
 show collections
 /***to find all values of the collection**/
 db.collection_name_here.find().pretty();
+/***remote acess**/
+sudo ufw allow from remote_machine_ip to any port 27017
+sudo ufw reload
