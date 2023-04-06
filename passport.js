dbf660566c2840299fd0b4ed8d0b54ab5d4c8a8d
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-04-06 16:12:34 +0300

    Update passport.js

--- passport.js
+++ passport.js
@@ -2,3 +2,16 @@
 https://www.passportjs.org/concepts/authentication/downloads/html/ */
 /***  https://github.com/zachgoll/express-jwt-authentication-starter/tree/final   **/
 
+/*** t h e   l o c a l   s t r a t e g y***/
+
+/***
+A client send a name and a password => server
+client         <=   server validate them and create
+                    a session in the DB.
+                    Sends a sessionID as a cookie
+client request =>   server
+client          <=  verify the cookie and send 
+                    back a response.When fail - 
+                    sends 403.
+
+***/
