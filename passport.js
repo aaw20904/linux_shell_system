f7c939da9353937bcc0de6951b1ce6c4a3ea1f23
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-04-06 11:33:22 +0300

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,3 +1,4 @@
 /* https://youtu.be/F-sFp_AvHc8 01:50 02:37 https://www.passportjs.org/docs/
 https://www.passportjs.org/concepts/authentication/downloads/html/ */
+/***  https://github.com/zachgoll/express-jwt-authentication-starter/tree/final   **/
 
