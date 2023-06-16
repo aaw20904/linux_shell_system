322d5b4ebf52c1b5b359e5e5532cd7a795414f44
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2023-06-16 21:06:03 +0300

    Create puppeteer.js

---
+++ puppeteer.js
@@ -0,0 +1,25 @@
+// https://ploi.io/documentation/server/how-to-install-puppeteer-on-ubuntu
+/**
+options to run on Linux server 
+*/
+
+puppetteer.launch({
+   headless: true,
+   args: ['--no-sandbox']
+})
+
+/**
+$ sudo npm install -g puppeteer 
+
+$ sudo apt-get install chromium-browser 
+ 
+$ sudo apt-get install libx11-xcb1 libxcomposite1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 
+
+$
+$
+$
+
+
+
+
+**/
