f0889f0511b9c199648401ff79708f4f759d6544
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2024-01-05 11:34:23 +0200

    Update javascript_prog_lang.js

--- javascript_prog_lang.js
+++ javascript_prog_lang.js
@@ -9,10 +9,13 @@ It allows store chunks of data effectively and therefore suitable for large file
 The structure.
 */
 
-/*You can crteate a blob from an array. */
-let blob = new Blob(["<html>…</html>"], {
-	type: 'text/html'
-});
+new Blob(blobParts);
+new Blob(blobParts, options);
+
+/* The first parameter must be iterable,
+the second has two optional fields:
+{type ,endings}, "type" contains MIME type*/
+
 //------------PROPERTIES:
 
     blob.size   //size of blob in bytes - readonly
@@ -21,7 +24,7 @@ let blob = new Blob(["<html>…</html>"], {
 //-------M E T H O D S:
 / *1) Converts into ArrayBuffer (returns Promise)  */
       
-    blob.ArrayBuffer()
+    blob.ArrayBuffer();
 
 /* 2) Returns a new Blob as slice of existing.
   There are avaliable 4 prototypes:  */
@@ -33,5 +36,68 @@ let blob = new Blob(["<html>…</html>"], {
 
 /* 3)Returns a readable stream:  */
 
-    blob.stream()
+    blob.stream();
+
+/* 4)Converting a blob to UTF-8 string. Returns promise */
+
+   blob.text();
+
+/*
+
+/*
+█▄▄ █▀█ █▀█ █░█░█ █▀ █▀▀ █▀█   █▀▀ █ █░░ █▀▀   █ █▄░█ █▀█ █░█ ▀█▀
+█▄█ █▀▄ █▄█ ▀▄▀▄▀ ▄█ ██▄ █▀▄   █▀░ █ █▄▄ ██▄   █ █░▀█ █▀▀ █▄█ ░█░
+*/
+
+// You can have acces to file/ file list using "change" event:
+ htmlInput.addEventListener("change", (input)=>{
+	 console.log(input.files[0]);
+ });
+//The file objects have the same properties as the instance of Blob
+
+█▀▀ █ █░░ █▀▀  █▀█ █▀▀ ▄▀█ █▀▄ █▀▀ █▀█
+█▀░ █ █▄▄ ██▄  █▀▄ ██▄ █▀█ █▄▀ ██▄ █▀▄
+*/
+ ///constructor 
+	FileReader();
+//instance properties:
+	FileReader.error;
+	FileReader.readyState; 
+	FileReader.result;
+//METHODS:
+	FileReader.result();
+//Reads data. Parameter - is the Blob or File from which to read.
+	FileReader.readAsArrayBuffer(blob);
+//
+	FileReader.readAsDataURL(blob);
+//
+	readAsText(blob);
+	readAsText(blob, encoding);
+/*---------- E V E N T S -------*/
+ // "abort" - fired when a read has been aborted
+	 addEventListener("abort", (event) => {});
+	 reader.onabort = (event) => {};
+// "error" 
+	addEventListener("error", (event) => {});
+	onerror = (event) => {};
+// "load" - The load event is fired when a file has been read successfully.
+	addEventListener("load", (event) => {});
+	onload = (event) => {};
+        //you can read loaded data  from the FileReader.result property
+
+//"loadend"  The loadend event is fired when a file read has completed, successfully or not.
+	addEventListener("loadend", (event) => {});
+	onloadend = (event) => {};
+//The "loadstart" event is fired when a file read operation has begun.
+// The "progress" event is fired periodically as the FileReader reads data.
+
+
+
+
+
+
+
+
+
+
 
