2beab77b96f4e8b24bdf5c430978b9a2b4923f7e
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2024-01-10 12:48:33 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -43,3 +43,71 @@ ctx.beginPath();
   context.bezierCurveTo(200, 5, 150, 20, 170, 80);
       // complete custom shape
   context.closePath();
+
+/*
+█▀█ █▀▀ █▀▀ ▀█▀ ▄▀█ █▄░█ █▀▀ █░░ █▀▀
+█▀▄ ██▄ █▄▄ ░█░ █▀█ █░▀█ █▄█ █▄▄ ██▄*/
+     context.beginPath();
+      context.rect(188, 50, 200, 100);
+      context.fillStyle = 'yellow';
+    //---fill inner area of a rect
+      context.fill();
+      context.lineWidth = 7;
+      context.strokeStyle = 'black';
+    //---draw rectangle
+      context.stroke();
+
+/*
+
+█▀▀ █▀█ ▄▀█ █▀▄ █ █▀▀ █▄░█ ▀█▀   ▄▀█ █▀   █▀▀ █ █░░ █░░
+█▄█ █▀▄ █▀█ █▄▀ █ ██▄ █░▀█ ░█░   █▀█ ▄█   █▀░ █ █▄▄ █▄▄
+*/
+     // add LINEAR gradient- 
+      var grd = ctx.createLinearGradient(188, 50, 200, 100);
+      // light blue
+      grd.addColorStop(0, '#8ED6FF');   
+      // dark blue
+      grd.addColorStop(1, '#004CB3');
+      ctx.fillStyle = grd;
+
+///or add RADIAL gradient-
+    // create radial gradient
+      var grd = context.createRadialGradient(238, 50, 10, 238, 50, 300);
+      // light blue
+      grd.addColorStop(0, '#8ED6FF');
+      // dark blue
+      grd.addColorStop(1, '#004CB3');
+      context.fillStyle = grd;
+
+//or load a custom P A T T E R N  - for example an image:
+
+      var imageObj = new Image();
+      imageObj.onload = function() {
+        var pattern = ctx.createPattern(imageObj, 'repeat');
+
+        ctx.rect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = pattern;
+        ctx.fill();
+      };
+      imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/wood-pattern.png';
+
+/*
+
+█▀▄ █▀█ ▄▀█ █░█░█   █ █▀▄▀█ ▄▀█ █▀▀ █▀▀
+█▄▀ █▀▄ █▀█ ▀▄▀▄▀   █ █░▀░█ █▀█ █▄█ ██▄
+*/
+   var imageObj = new Image();
+
+      imageObj.onload = function() {
+       //optional - ajusting size of the canvas
+        canvas.width = imageObj.width+5;
+        canvas.height = imageObj.height+5;
+       //-----draw an image by default
+        ctx.drawImage(imageObj, 0, 0);
+       //---or draw an image with scalling
+        var width = 64;
+         var height = 40;
+        ctx.drawImage(imageObj, 0, 0, width, height);
+      };
+      imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+    
