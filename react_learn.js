0c8df9fdaf991fa5ea0c5801c969a17cda0b56de
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2024-04-25 10:55:55 +0300

    Update react_learn.js

--- react_learn.js
+++ react_learn.js
@@ -1,1 +1,19 @@
-rea
+/* React component names MUST always start with a capital letter, while HTML tags must be lowercase.
+For example:*/
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
