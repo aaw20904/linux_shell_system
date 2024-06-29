b72bfb8bbc87e442710e84997dafc98221a255b8
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2024-06-29 13:46:20 +0300

    Update and rename react_learn.js to react_learn.jsx

--- react_learn.jsx
+++ react_learn.jsx
@@ -49,7 +49,10 @@ because we can`t change sataeVariable directly (it must be immutable).
 To renew object, use ... spread operator and re-define properties**/
   stateVarSetter({...stateVar,myProperty:"new_value"});
 
-
+/*
+█░█ █▀ █▀▀   █▀▀ █▀▀ █▀▀ █▀▀ █▀▀ ▀█▀     █░█ █▀█ █▀█ █▄▀
+█▄█ ▄█ ██▄   ██▄ █▀░ █▀░ ██▄ █▄▄ ░█░     █▀█ █▄█ █▄█ █░█
+*/
 useEffect(setup, dependencies?) 
 /* you can only call it at the top level of your component or your
 own Hooks. You can’t call it inside loops or conditions.*/
@@ -60,4 +63,55 @@ useEffect(()=>{  /*runs when a component had been added*/
                   return ()=>{ /*optionally, runs when component is removed from the DOM*/ }
               },  [x]);
 
-//To run once , insert an empty array in "dependencies"
+//To run once , insert an empty array in "dependencies".There may be any state variables
+
+/*
+
+█▀█ █▀▀ ▄▀█ █▀▀ ▀█▀ ▄▄ █▀█ █▀█ █░█ ▀█▀ █▀▀ █▀█ ▄▄ █▀▄ █▀█ █▀▄▀█   █░░ █ █▄▄ █▀█ ▄▀█ █▀█ █▄█
+█▀▄ ██▄ █▀█ █▄▄ ░█░ ░░ █▀▄ █▄█ █▄█ ░█░ ██▄ █▀▄ ░░ █▄▀ █▄█ █░▀░█   █▄▄ █ █▄█ █▀▄ █▀█ █▀▄ ░█░
+*/
+
+// It is SHORT, NOT FULLY description
+/* 
+This library allows to create routes inside your REACT project.For example , we need to create three routes :"/home","/menu","/contact".
+ THe contact.jsx file:*/
+
+ export const Contact =() =>{
+    return <h1>Contact page</h1>
+}
+/*To create the Navigation panel - here are the "Link" or "NavLink" objects , that we must imported.Here is the <Navbar> component: */
+
+import {NavLink} from "react-router-dom"
+
+export const Navbar = () =>{
+    return (
+        <div className="d-flex flex-row justify-content-start align-items-center nav  p-0">
+      /*When we are inside page with the same link - isActive is TRUE*/
+            <NavLink  className={( {isActive} ) =>  isActive ? "ref-active p-2"  : "ref p-2"  } to="/">HOME </NavLink>
+            <NavLink  className={( {isActive}  ) =>  isActive ? "ref-active p-2" : "ref p-2"  } to="/menu" isActive="text-danger">menu </NavLink>
+            <NavLink className={( {isActive}  ) =>  isActive ? "ref-active p-2" : "ref p-2"  } to="/contact">contact </NavLink>
+        </div>
+    )
+}
+
+/*
+To create three pages, we create three files "Home.jsx","Menu.jsx","Contact.jsx" where we creating apges and import them as Rect components.
+After this , we insert exported components snside th structure in the Main file*/
+  import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+   <Router>
+      <div>
+      <h5>navbar</h5>
+      <Navbar />
+      </div>
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<p>Page not found</p>}/>
+     </Routes>
+   </Router>
+
+
+/*The navigation menu can be created by the "Link" or "NavLink" objects.
+*/
+
