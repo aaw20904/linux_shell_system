4ecde22e017f449c13af1ddf6effa87b178cfd1d
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2024-07-01 16:10:49 +0300

    Update react_learn.jsx

--- react_learn.jsx
+++ react_learn.jsx
@@ -240,5 +240,61 @@ const Test =()=>{
 }
 
 export {Test}
+    /*
     
+█▀█ █▀▀ ▄▀█ █▀▀ ▀█▀ ▄▄ █░█ █▀█ █▀█ █▄▀ ▄▄ █▀▀ █▀█ █▀█ █▀▄▀█       █▄█ █░█ █▀█
+█▀▄ ██▄ █▀█ █▄▄ ░█░ ░░ █▀█ █▄█ █▄█ █░█ ░░ █▀░ █▄█ █▀▄ █░▀░█       ░█░ █▄█ █▀▀
 
+npm install react-hook-form  yup  @hookform/resolvers
+    */
+
+/*To validate form info. 'react-hook-form' for validation
+'yup' for validation schema create,
+'@hookform/resolvers' to resolve validation
+ Example of a component-form:*/
+
+import {useForm} from "react-hook-form"
+import {yupResolver} from "@hookform/resolvers/yup"
+import * as yup from 'yup'
+
+const MyForm = () =>{
+
+///-------------the schema for validation
+   const schema =yup.object().shape({
+    fullName: yup.string().required("Your name is required!"),
+     ///---you write directly type of data, error message
+    email:yup.string().email().required("Your mail must be correct!"),
+    age: yup.number().positive().integer().min(18).required("Your age must be greater that 18"),
+    password: yup.string().min(4).max(20).required('Password required'),
+     ///condition - both passwords must be match
+    confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords must be match!").required(),
+   })
+
+     const {register, handleSubmit, formState: {errors} } = useForm({
+      resolver: yupResolver(schema)
+   });
+
+   const onSubmit =(data)=>{
+    console.log(data)
+   }
+    return (  
+    <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
+        ///-----register each input
+        <input type="text" placeholder='full  name..' {...register("fullName")}/>
+        ///-------when error exists - show it
+            <p>{errors.fullName?.message}</p>
+        <input type="text" placeholder='email..' {...register("email")} />
+            <p>{errors.email?.message}</p>
+        <input type="number" placeholder='age...'  {...register("age")} />
+            <p>{errors.age?.message}</p>
+        <input type="password" placeholder='password' {...register("password")}/>
+            <p>{errors.password?.message}</p>
+         <input type="password" placeholder="confirm pwd"  {...register("confirmPassword")}/>
+            <p>{errors.confirmPassword?.message}</p>
+        <input type="submit" placeholder="confirm pwd"/>
+    </form>
+    ) 
+}
+
+export {MyForm}
+ 
