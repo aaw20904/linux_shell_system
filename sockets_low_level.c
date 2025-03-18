d6d4deeb5dbc278448d075d16f2bbf2bdae1cfd4
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-03-18 14:24:56 +0200

    Update sockets_low_level.c

--- sockets_low_level.c
+++ sockets_low_level.c
@@ -1,1 +1,17 @@
-
+/****MICROSOFT WINDOWS environment***
+ #    # # #    # #####   ####  #    #  ####  
+ #    # # ##   # #    # #    # #    # #      
+ #    # # # #  # #    # #    # #    #  ####  
+ # ## # # #  # # #    # #    # # ## #      # 
+ ##  ## # #   ## #    # #    # ##  ## #    # 
+ #    # # #    # #####   ####  #    #  #### 
+FLOWCAHRT
+1. Winsock Initialization:  WSAStartup()
+2. Creating a Socket:   socket()
+3. Binding the Socket:  bind()
+4. Listening for Connections (it allows incoming conections):  listen()
+5. Accepting a Connection (incoming client`s connection):  accept()
+6. Sending & Receiving Data with functions:  send() and recv()
+7. Closing Sockets:  closesocket(clientSocket); closesocket(serverSocket);
+8.Clean up WSA: WSACleanup();
+*/
