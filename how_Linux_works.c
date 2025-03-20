feba2261c9af71e5afb02a3b427784ca516475bf
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-03-20 15:06:11 +0200

    Update how_Linux_works.c

--- how_Linux_works.c
+++ how_Linux_works.c
@@ -1,1 +1,14 @@
+/*
 
+█▀█ █▀█ █▀ █ ▀▄▀
+█▀▀ █▄█ ▄█ █ █░█
+ (Portable Operating System Interface) 
+ 
+open()	    Opens a device or file (/dev/ttyAMA0)
+close()	    Closes the file descriptor
+read()	    Reads data from UART (or a file)
+write()	    Sends data to UART (or a file)
+tcgetattr()	Gets UART port settings (baud rate, parity, etc.)
+tcsetattr()	Sets UART port settings
+tcflush()	  Clears the UART buffer
+**/
