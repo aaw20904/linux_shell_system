66c6c6e5eaa3999a6b27357e107018cfad23f458
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-03-25 15:34:36 +0200

    Update how_Linux_works.c

--- how_Linux_works.c
+++ how_Linux_works.c
@@ -170,6 +170,8 @@ tcpFd = socket(AF_INET, SOCK_STREAM, 0);
           perror("Socket failed");
           exit(EXIT_FAILURE);
   }
-//2)Bind 
-
+ // 2) Bind socket
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
