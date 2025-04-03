f2940425efe723c30c4cd878951797c7e357a569
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-04-03 14:14:52 +0300

    Update assembling_NASM.asm

--- assembling_NASM.asm
+++ assembling_NASM.asm
@@ -1,1 +1,5 @@
-
+;-----------COMPILE--LINK--------------------------------
+;to compile - use the folowing command:
+;   D:\NASM>nasm -f win32 3.asm -o 3.obj
+;to link with MyLink - use the following command:
+;   D:\NASM>GoLink /entry main /console 3.obj msvcrt.dll kernel32.dll
