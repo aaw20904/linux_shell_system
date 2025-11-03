d0893cadf2438018df5397fae9823a8a356e6e41
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-11-03 19:57:59 +0200

    Update LinuxNotes.c

--- LinuxNotes.c
+++ LinuxNotes.c
@@ -2,22 +2,51 @@
 /*
 Create  an executable file in Ubuntu:
 1) compile
-   gcc myprogram.c -o myprogram.o
+   gcc -c myprogram.c -o myprogram.o
 2) link and build
-   gcc myprogram.o myprogram
+   gcc myprogram.o -o myprogram
 3) Change privileges and mode (enable execute)
   sudo chmod 777 myprogram
 4)run
   ./myprogram
 **/
+/*OPTIONAL:  create assembly translation:
+   gcc -O0 -m32 -fverbose-asm -S test.c -o test.s
+*/
 
 /*******create shared library .SO (like .DLL in Windows)
 
   1)Compile it as position-independent code. -fPIC → generates position-independent code, 
   which can be loaded at any memory address (required for .so).
-gcc -fPIC -c mymath.c
+gcc -fPIC -c mylib.c
   2)Create the shared library
-  gcc -shared -o libmymath.so mymath.o
-3)
+  gcc -shared -o libmymath.so mylib.o
+3) you can open it dynamically like this:
 
 *****/
+
+//for example, here is the shared library body, there are only functions, without any "main(){...}":
+
+  int divBy2(int a){
+   return (a >> 1);
+ }
+
+///here is the example,  how to open the shared library:
+#include <stdio.h>
+#include <dlfcn.h>
+ 
+int main(void){
+   //1)open the library
+  void* handle = dlopen("./mysharedlib.so", RTLD_LAZY);
+    if (!handle) {
+        printf("Error: %s\n", dlerror());
+        return 1;
+    }
+   //2)get the address of the procedure:
+   int (*divBy2)(int)= dlsym(handle,"divBy2");
+    //3)call the library`s function:
+ printf("Hello word %d \n", divBy2(16));
+   //4)close the shared library
+ dlclose(handle);
+ return 0;
+}
